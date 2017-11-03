1329470cf870948f4632e1b1b378559c11799f3a
khaled <elgendy1204@gmail.com>
2017-11-03 16:49:59 +0200

    add feednami service for reading rss data

--- src/js/components/maincontent/FeedDetailsArea.js
+++ src/js/components/maincontent/FeedDetailsArea.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import { Scrollbars } from 'react-custom-scrollbars';
 import FeedDetails from './FeedDetails';
+import PropTypes from 'prop-types';
 
 class FeedDetailsArea extends Component {
+
+	constructor(props){
+		super(props);
+	}
+
+	loadXml(){
+		feednami.load(this.props.activeFeed.url).then(feed => {
+			console.log(feed);
+		});
+	}
+
 	render() {
+		this.loadXml();
 		return (
 			<div className='feed-details-area'>
 				<Scrollbars>
@@ -23,5 +36,9 @@ class FeedDetailsArea extends Component {
 		);
 	}
 }
+
+FeedDetailsArea.propTypes = {
+	activeFeed: PropTypes.object.isRequired
+};
 
 export default FeedDetailsArea;
